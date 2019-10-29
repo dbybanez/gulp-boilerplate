92d296b5c9670d07cb76bc3595cd6b68e775fdc8
David Benjamin Ybañez <david.ybaez@lexmark.com>
2019-10-29 18:42:37 +0800

    restructured scss files, 'fixed' gulp bug when compiling from two different sources

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,44 @@ const browserSync = require("browser-sync").create();
 const cleanCSS    = require('gulp-clean-css');
 const rename      = require("gulp-rename");
 
+sass.compiler = require('node-sass');
+
 // Compile SCSS into CSS
 
 function style() {
-  // 1. location of SCSS files
-  return gulp.src('app/scss/**/*.scss')
+  // 1. where is my scss files or 'app/scss/**/*.scss'
+  return gulp.src('app/scss/app.scss', {sourcemaps: true})
   // 2. pass that file through the sass compiler
     .pipe(sass().on('error', sass.logError))
   // 3. rename the output
     .pipe(rename('custom-style.css'))
-  // 4. location of saved CSS file
-    .pipe(gulp.dest('app/css'))
+  // 4. where do i save the compiled CSS?
+    .pipe(gulp.dest('app/css', {sourcemaps: true}))
   // 5. minify and check compatibility
     .pipe(cleanCSS({compatibility: 'ie8'}))
-  // 6. rename to .min (must use .min.css in index.html)
+  // 6. rename to .min
     .pipe(rename({suffix: '.min'}))
   // 7. add to app/css directory
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('app/css', {sourcemaps: true}))
+  // 8. stream changes to all browsers
+    .pipe(browserSync.stream())
+}
+
+function bootstrap() {
+  // 1. where is my scss files or 'app/scss/**/*.scss'
+  return gulp.src('app/scss/bootstrap.scss', {sourcemaps: true})
+  // 2. pass that file through the sass compiler
+    .pipe(sass().on('error', sass.logError))
+  // 3. rename the output
+    .pipe(rename('custom-bootstrap.css'))
+  // 3. where do i save the compiled CSS?
+    .pipe(gulp.dest('app/css', {sourcemaps: true}))
+  // 5. minify and check compatibility
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+  // 6. rename to .min
+    .pipe(rename({suffix: '.min'}))
+  // 7. add to app/css directory
+    .pipe(gulp.dest('app/css', {sourcemaps: true}))
   // 8. stream changes to all browsers
     .pipe(browserSync.stream())
 }
@@ -32,9 +53,9 @@ function watch() {
     }
   });
   gulp.watch('app/scss/**/*.scss', style);
+  gulp.watch('app/scss/**/*.scss', bootstrap);
   gulp.watch('app/*.html').on('change', browserSync.reload);
   gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
-  
 }
 
 exports.style = style;
